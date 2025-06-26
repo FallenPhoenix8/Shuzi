a1bac153c95666e02307e931e845374f367b3c75
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-06-26 22:17:08 +0200

    Initial Commit

---
+++ Shuzi/ShuziApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuziApp.swift
+//  Shuzi
+//
+//  Created by Łukasz Kwiecień on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShuziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
